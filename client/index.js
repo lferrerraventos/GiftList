05ac13e94f9de9c25179724cf5c20357aedccc48
Luis <luis@bootdevelop.com>
2023-12-15 08:46:36 +0100

    Week 3 Project Submission

--- client/index.js
+++ client/index.js
@@ -3,15 +3,31 @@ const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
+var args = process.argv.slice(2);
+
+if(args.length === 0) {
+  throw new Error("You have to provide a name");
+}
+
+const name = args.join(" ");
+
+const merkleTree = new MerkleTree(niceList);
+
+console.log("Hi " + name);
+
+const index = niceList.findIndex((n) => n === name);
+const proof = merkleTree.getProof(index);
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
-
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
-
-  console.log({ gift });
+  if (index !== -1) {
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+      proof,
+      name,
+    });
+    console.log(gift);
+  } else {
+    console.log("Error: You are not on the list :(");
+  }
 }
 
 main();
